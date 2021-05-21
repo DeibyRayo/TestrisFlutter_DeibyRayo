5b558a893c111fd86bf19091d745c36bc3b100d5
DeibyRayo <deibyrayo95@gmail.com>
2021-05-20 21:19:10 -0500

    flutter prueba

---
+++ lib/score_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ScoreBar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ScoreBarState();
+}
+
+class _ScoreBarState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          end:
+              Alignment(0.9, 0.0), // 10% of the width, so there are ten blinds.
+          colors: const <Color>[
+            Color(0xFF283593),
+            Color(0xFF283593)
+          ], // red to yellow
+          tileMode: TileMode.repeated, // repeats the gradient over the canvas
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              'Score: 0',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
